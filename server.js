c9f509aac57e0b09b51ff3e9d5189796c37c3e63
Hibah Shafi <hibahshafi.hs@gmail.com>
2020-01-30 17:51:01 -0600

    Added the packages, scarping working

---
+++ server.js
@@ -0,0 +1,78 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "NYT";
+var collections = ["articles"];
+
+//build collection and add records 
+//scrape function 
+//displaying scarp info on page
+//save functionality 
+//add note functionality (form and post to DB)
+//Delete functionilty 
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function (req, res) {
+    res.send("Hello world");
+});
+
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function (req, res) {
+    // Make a request via axios for the news section of `ycombinator`
+    axios.get("https://www.nytimes.com/section/world").then(function (response) {
+
+        // Load the HTML into cheerio
+        var $ = cheerio.load(response.data);
+        // Make an empty array for saving our scraped info
+        var results = [];
+        // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
+        $("div.css-1l4spti").each(function (i, element) {
+            //pushing title and link of the article into results array from NYT times 
+            var article = $(element).text();
+            var link = $(element).children().attr("href");
+            results.push({
+                article: article,
+                link, link
+            });
+        });
+
+        // After looping through each element found, log the results to the console
+        console.log(results);
+
+
+        // Send a "Scrape Complete" message to the browser
+        //change to send to articl.ehandlabrs 
+        res.send("Scrape Complete");
+    });
+});
+
+// Listen on port 3000
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
+});
